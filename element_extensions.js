e1df944ae54c12a3d37456d2c97868a91678de49
igor.rosenberg <igor.rosenberg@gmail.com>
2015-03-01 18:37:55 +0100

    separate files for JS modules (+ JSHint)

---
+++ element_extensions.js
@@ -0,0 +1,35 @@
+
+var element_extensions = (function(){ 
+    return { 
+		// Adding methods to all DOM Elements
+		init: function() {
+			Element.prototype.toggleDisplay = Element.prototype.toggleDisplay || function () { 
+			   this.style.display = (this.style.display === 'none' || this.style.display === '' ? 'block' : 'none' ); 
+			};
+
+			// Toggle the display of all the {siblings of the considered DOM Element which have the specified class}  
+			Element.prototype.toggleDisplayToSiblingIfClass = Element.prototype.toggleDisplayToSiblingIfClass || function (className) {
+			  var elements = this.parentNode.getElementsByClassName(className);
+			  for (var i=0; i < elements.length ; i++) {
+				   elements[i].toggleDisplay();
+			  }
+			};
+
+			// @returns whether the considered DOM Element has the specified class 
+			Element.prototype.hasClass = Element.prototype.hasClass || function (cls) {
+			  return (' ' + this.className + ' ').indexOf(' ' + cls + ' ') > -1;
+			};
+
+			// ex : document.head.addJavascript("function f() { alert(); }") ;
+			Element.prototype.addJavascript = Element.prototype.addJavascript || function (scriptString) {
+			  var s = document.createElement('script');
+			  s.setAttribute('type','text/javascript');
+			  s.text = scriptString;
+			  this.appendChild(s);
+			};
+    	} 
+	};
+})();
+
+
+
