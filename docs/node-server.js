44fd1a91b2af5510b9d630d1941dea29312d9c3f
igorrosenberg <igor.rosenberg@gmail.com>
2019-03-23 20:23:46 +0100

    node-server with folder as param[0]

--- docs/node-server.js
+++ docs/node-server.js
@@ -1,9 +1,14 @@
+// HTTP Server on port 8000, args[0] being the folder to serve
+// node node-server.js 
+
 var http = require('http');
 
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./");
+var path = process.argv.length > 2? process.argv[2] :  "./";
+var serve = serveStatic(path);
+
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
